a01b1814376288a3f18cd072de6f7983eee0dffd
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-12 02:21:34 +0100

    make icon svg hoverable

--- src/components/password-generator/password-display.tsx
+++ src/components/password-generator/password-display.tsx
@@ -1,18 +1,22 @@
-import copyIcon from '../../assets/icon-copy.svg'
+import IconCopy from "../../assets/icon-copy";
 
-const PasswordDisplay = ({password}: PasswordDisplayProps) => {
-    const isPasswordNotEmpty = password !== ''
+const PasswordDisplay = ({ password }: PasswordDisplayProps) => {
+  const isPasswordNotEmpty = password !== "";
 
-    return (
-        <div className={`text-almost-white  px-4 py-4 bg-dark-grey w-full flex justify-between items-center text-heading-M`}>
-            <span className={`${isPasswordNotEmpty ? '' : 'opacity-25'}`}>{isPasswordNotEmpty ? password : 'P4$5W0rD!'}</span>
-            <button><img src={copyIcon} alt="" className='inline w-4 h-5'/></button>
-        </div>
-    )
-}
+  return (
+    <div
+      className={`text-almost-white  px-4 py-4 bg-dark-grey w-full flex justify-between items-center text-heading-M`}
+    >
+      <span className={`${isPasswordNotEmpty ? "" : "opacity-25"}`}>
+        {isPasswordNotEmpty ? password : "P4$5W0rD!"}
+      </span>
+      <IconCopy className="text-neon-green hover:text-almost-white" />
+    </div>
+  );
+};
 
 type PasswordDisplayProps = {
-    password: string
-}
+  password: string;
+};
 
-export default PasswordDisplay
+export default PasswordDisplay;
