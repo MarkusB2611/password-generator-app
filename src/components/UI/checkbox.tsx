92bec19cc7738889581997fd2faf0a934acb0aec
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-12 01:51:41 +0100

    custom checkbox

--- src/components/UI/checkbox.tsx
+++ src/components/UI/checkbox.tsx
@@ -1,7 +1,7 @@
 const Checkbox = ({ id, label }: CheckboxProps) => {
   return (
     <div className="flex gap-5 items-center mt-4 first:mt-12">
-      <input className="accent-neon-green h-5 w-5" type="checkbox" id={id} />
+      <input className="checkbox" type="checkbox" id={id} />
       <label className="text-base" htmlFor={id}>
         {label}
       </label>
