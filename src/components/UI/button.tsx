6c05b592cb092939bc059b5a586e0a2313334205
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-12 12:48:47 +0100

    button mobile

--- src/components/UI/button.tsx
+++ src/components/UI/button.tsx
@@ -1,5 +1,7 @@
+import arrow from '../../assets/icon-arrow-right.svg'
+
 const Button = ({children} : ButtonProps) => {
-    return <button className="bg-neon-green text-dark-grey py-[1.125rem] sm:py-5 w-full font-jetbrainsmono">{children}</button>
+    return <button className="bg-neon-green text-dark-grey py-[1.125rem] sm:py-5 w-full font-jetbrainsmono flex justify-center items-center gap-4"><span>{children.toUpperCase()}</span> <img className='inline' src={arrow} alt="" /></button>
 }
 
 type ButtonProps = {
