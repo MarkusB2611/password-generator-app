3c1a08bab35e6725f36222e755b85c66e42dafdf
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-13 12:47:52 +0100

    button hover and disabled states

--- src/components/UI/button.tsx
+++ src/components/UI/button.tsx
@@ -1,11 +1,12 @@
-import arrow from '../../assets/icon-arrow-right.svg'
+import ArrowRight from "../../assets/icon-arrow-right"
 
-const Button = ({children} : ButtonProps) => {
-    return <button className="bg-neon-green text-dark-grey py-[1.125rem] sm:py-5 w-full font-jetbrainsmono flex justify-center items-center gap-4"><span>{children.toUpperCase()}</span> <img className='inline' src={arrow} alt="" /></button>
+const Button = ({children, disabled} : ButtonProps) => {
+    return <button className="bg-neon-green text-dark-grey py-[1.125rem] sm:py-5 w-full font-jetbrainsmono flex justify-center items-center gap-4 border-2 box-border border-dark-grey enabled:hover:bg-dark-grey enabled:hover:text-neon-green enabled:hover:border-neon-green disabled:bg-grey disabled:cursor-not-allowed" disabled={disabled}><span>{children.toUpperCase()}</span><ArrowRight className="text-inherit" /></button>
 }
 
 type ButtonProps = {
     children: string
+    disabled: boolean
 }
 
 export default Button
