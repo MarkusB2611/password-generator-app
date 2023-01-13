3633927a75c9a4cd7854d61d8d23b951f8fa2026
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-13 12:33:45 +0100

    first password strength logic

--- src/components/password-generator/password-settings.tsx
+++ src/components/password-generator/password-settings.tsx
@@ -10,6 +10,20 @@ const PasswordSettings = ({
   changeSettings,
 }: PasswordSettingsProps) => {
 
+  const calculatePasswordStrength = () => {
+    if(!settings.useUppercase && !settings.useLowercase && !settings.useNumbers && !settings.useSymbols) return 0
+    if(settings.length === 0) return 0;
+    let score = 0;
+    if(settings.length >= 1 && settings.length < 5) score +=1
+    if(settings.length >= 5 && settings.length < 8) score +=2
+    if(settings.length >= 8 && settings.length < 15) score +=3
+    if(settings.length >= 15) score +=4
+    if(settings.useUppercase) score +=1
+    if(settings.useLowercase) score +=1
+    if(settings.useNumbers) score +=1
+    if(settings.useSymbols) score +=1
+    return Math.floor(score/2)
+  }
 
   return (
     <div className="w-full bg-dark-grey p-4 flex flex-col gap-8">
@@ -44,9 +58,9 @@ const PasswordSettings = ({
         setValue={changeSettings}
       />
       </div>
-      <PasswordStrength calculatedStrength={0} />
+      <PasswordStrength calculatedStrength={calculatePasswordStrength()} />
       
-      <Button>Generate</Button>
+      <Button disabled={calculatePasswordStrength() === 0}>Generate</Button>
     </div>
   );
 };
